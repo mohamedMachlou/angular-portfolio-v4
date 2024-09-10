1624bfbc77b2c7e496bff433220998aece389780
Mohamed Machlou <medmachlou@gmail.com>
2024-09-10 16:29:17 +0100

    about animation numbers

--- src/app/portfolio/components/about/about.component.ts
+++ src/app/portfolio/components/about/about.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
-export class AboutComponent {}
+export class AboutComponent implements OnInit {
+  interval = 160;
+  yearsMax = 15;
+  nbWebsiteMax = 200;
+  nbClientsMax = 500;
+
+  years = signal<number>(0);
+  nbWebsites = signal<number>(0);
+  nbClients = signal<number>(0);
+
+  ngOnInit(): void {
+    this.valuesAnimation();
+  }
+  valuesAnimation() {
+    if (this.nbClients() < this.nbClientsMax) {
+      setInterval(() => {
+        if (this.years() < this.yearsMax) {
+          this.years.set(this.years() + 1);
+        }
+        if (this.nbWebsites() < this.nbWebsiteMax) {
+          this.nbWebsites.set(this.nbWebsites() + 10);
+        }
+        if (this.nbClients() < this.nbClientsMax) {
+          this.nbClients.set(this.nbClients() + 25);
+        }
+      }, this.interval);
+    }
+  }
+}
