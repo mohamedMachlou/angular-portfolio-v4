b9cf121250eedd9ad3808e04f36c1b484a34a0f4
Mohamed Machlou <medmachlou@gmail.com>
2024-09-14 01:55:13 +0100

    contact form validate all fields

--- src/app/portfolio/components/contact/contact.component.ts
+++ src/app/portfolio/components/contact/contact.component.ts
@@ -12,18 +12,58 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  options = [
+    'Electrical',
+    'HVAC',
+    'Plumbing',
+    'Roofing',
+    'Garage Door',
+    'Solar',
+    'Other',
+  ];
   form = new FormGroup({
     firstname: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
       Validators.maxLength(40),
-      Validators.pattern('[a-zA-Z]+'),
+      Validators.pattern('[a-zA-Z ]+'),
     ]),
     lastname: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
       Validators.maxLength(40),
+      Validators.pattern('[a-zA-Z ]+'),
+    ]),
+    companyname: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(40),
+      Validators.pattern('[a-zA-Z ]+'),
+    ]),
+    option: new FormControl('', [Validators.required]),
+    phonenumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(9),
+      Validators.maxLength(15),
+      Validators.pattern('[0-9 ]+'),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(40),
+      Validators.pattern('[a-zA-Z0-9_.]+@[a-zA-Z]+.[a-zA-Z]+'),
+    ]),
+    state: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(35),
       Validators.pattern('[a-zA-Z]+'),
+    ]),
+    message: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(1000),
+      Validators.pattern('[a-zA-Z0-9_., ]+'),
     ]),
   });
 
@@ -33,10 +73,28 @@ export class ContactComponent {
   get lastname() {
     return this.form.get('lastname');
   }
+  get companyname() {
+    return this.form.get('companyname');
+  }
+  get option() {
+    return this.form.get('option');
+  }
+  get phonenumber() {
+    return this.form.get('phonenumber');
+  }
+  get email() {
+    return this.form.get('email');
+  }
+  get state() {
+    return this.form.get('state');
+  }
+  get message() {
+    return this.form.get('message');
+  }
 
   // submit method
 
   submit() {
-    console.log(this.firstname?.errors?.['minlength']);
+    console.log(this.form.value);
   }
 }
